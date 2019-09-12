d6489bd469cb582b0d5f015ad463339c0c9c5121
wangqin <380162347@qq.com>
2019-09-12 09:37:48 +0800

    地址选择开发

--- src/app/select-address/select-address.component.ts
+++ src/app/select-address/select-address.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, OnChanges, SimpleChanges, AfterContentChecked} from '@angular/core';
+declare var  AMap: any;
 @Component({
   selector: 'app-select-address',
   templateUrl: './select-address.component.html',
   styleUrls: ['./select-address.component.css']
 })
 export class SelectAddressComponent implements OnInit {
-  title: string;
+  isShorten: boolean;
+  isHide: boolean;
+  @ViewChild('cancel', {static: true}) cancel: any;
+
   constructor() { }
-  ngOnInit() {
-    this.title = '收货地址管理';
+  ngOnInit(): void {
+    this.isShorten = false;
+    this.isHide = true;
+    this.getMap ();
+  }
+
+  // 地图要放到函数里。
+  getMap() {
+    let map = new AMap.Map('container', {
+      resizeEnable: true,
+      zoom: 14,
+      scrollWheel: true, // 禁止鼠标滚轮缩放
+      center: [118.797500, 32.088500]
+    });
+
+  }
+
+  search(e: any) {
+    console.log(e);
+  }
+  onFocus(e: any) {
+    this.isShorten = true;
+    this.isHide = false;
+  }
+  getCancel() {
+    this.isShorten = false;
+    this.isHide = true;
   }
 }
